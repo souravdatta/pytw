7bd401bff4eb2e4a2511efc82d17fb980b3b327e
souravdatta <soura.jagat@gmail.com>
2015-06-12 01:10:31 +0530

    Initial Commit

---
+++ flask_app.py
@@ -0,0 +1,13 @@
+__author__ = 'Sourav Datta'
+
+from flask import *
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+if __name__ == '__main__':
+    app.run()
+
---
+++ static/pytw.js
@@ -0,0 +1,5 @@
+(function () {
+    // PyTw app
+
+
+})();
