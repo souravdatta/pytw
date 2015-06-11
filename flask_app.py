bcb6dee311f1a2886c6e812eb665772a0dd9b6d5
souravdatta <soura.jagat@gmail.com>
2015-06-12 02:22:57 +0530

    Addition of home page and URL

--- flask_app.py
+++ flask_app.py
@@ -1,12 +1,14 @@
 __author__ = 'Sourav Datta'
 
 from flask import *
+import authomatic
 from authomatic.adapters import WerkzeugAdapter
 from authomatic import Authomatic
 from config import CONFIG
 
 
 app = Flask(__name__)
+app.debug = True
 
 @app.route('/')
 def index():
@@ -14,7 +16,18 @@ def index():
 
 @app.route('/home')
 def home():
-    return render_template('home.html', user='Sourav')
+    return render_template('home.html', user='Hola')
+
+@app.route('/login/<provider_name>', methods=['GET', 'POST'])
+def login(provider_name):
+    response = make_response()
+    result = Authomatic(CONFIG, 'secret').login(WerkzeugAdapter(request, response), provider_name)
+    print(result)
+    if result:
+        if result.user:
+            result.user.update()
+            return render_template('home.html', user='Logged In')
+    return redirect('/')
 
 if __name__ == '__main__':
     app.run()
