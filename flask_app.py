4b0445b5598eb466216daafe18422bbe65a4d5a3
souravdatta <soura.jagat@gmail.com>
2015-06-12 10:56:11 +0530

    Now to Twitter auth

--- flask_app.py
+++ flask_app.py
@@ -24,9 +24,8 @@ def home():
     auth = tweepy.OAuthHandler(CONFIG['tw']['consumer_key'],
                                CONFIG['tw']['consumer_secret'])
     token = session['request_token']
-    del session['request_token']
-    #auth.request_token = token
-    auth.set_request_token(token[0], token[1])
+    #del session['request_token']
+    auth.request_token = token
     try:
         auth.get_access_token(verifier)
         session['access_token'] = auth.access_token
@@ -49,8 +48,7 @@ def login():
 
     try:
         redirect_url = auth.get_authorization_url()
-        #session['request_token'] = auth.request_token
-        session['request_token'] = (auth.request_token.key, auth.request_token.secret)
+        session['request_token'] = auth.request_token
         return redirect(redirect_url)
     except tweepy.TweepError as ex:
         print('Failed to get request token ', ex)
