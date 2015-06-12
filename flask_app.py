05c195157c6405966c80033de663f51b5e15b259
souravdatta <soura.jagat@gmail.com>
2015-06-12 10:01:56 +0530

    Now to Twitter auth

--- flask_app.py
+++ flask_app.py
@@ -8,9 +8,34 @@ from config import CONFIG
 app = Flask(__name__)
 app.debug = False
 
+api = None
+
 @app.route('/')
 def index():
     return render_template('index.html')
+
+@app.route('/home')
+def home():
+    args = request.args
+    if ('denied' in args) or ('request_token' not in session):
+        flash('Could not login, access denied!')
+        return redirect('/')
+    verifier = args['oauth_verifier']
+    auth = tweepy.OAuthHandler(CONFIG['tw']['consumer_key'],
+                               CONFIG['tw']['consumer_secret'])
+    token = session['request_token']
+    del session['request_token']
+    auth.request_token = token
+    try:
+        auth.get_access_token(verifier)
+        session['access_token'] = auth.access_token
+        session['access_token_secret'] = auth.access_token_secret
+        api = tweepy.API(auth)
+        api.update_status('Hello tweepy!')
+    except:
+        print('Error in access token')
+    finally:
+        return render_template('home.html')
 
 @app.route('/login')
 def login():
