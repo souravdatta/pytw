dc8595be0586ec6cc12e592f6c5fc1d2e98ab4c6
Sourav Datta <soura.jagat@gmail.com>
2015-06-12 13:47:05 +0530

    Removed keys for privacy after deploying to pythonanywhere

--- config.py
+++ config.py
@@ -1,11 +1,9 @@
 __author__ = 'Sourav Datta'
 
-from authomatic.providers import oauth1
-
 CONFIG = {
     'tw': {
-        'consumer_key': 'Hhc5by8DLQsW5SMEqeWKmF2N9',
-        'consumer_secret': 'U4wz4BeCE5XoFetD48rubYqYiAckjU3K5teuE2BfzPad297gkR'
+        'consumer_key': '<<REMOVED_FOR_PRIVACY>>',
+        'consumer_secret': '<<REMOVED_FOR_PRIVACY>>'
     }
 }
 
